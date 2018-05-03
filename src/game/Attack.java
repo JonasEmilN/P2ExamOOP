4696d2762ebae032dcccf83242286e88c0a5ab03
JonasEmilN /Main <Jonasemil91@gmail.com>
2018-05-04 01:49:10 +0200

    more attack

--- src/game/Attack.java
+++ src/game/Attack.java
@@ -1,4 +1,31 @@
 package game;
 
+import java.util.Random;
+
 public class Attack {
+
+    int combatValue;
+
+    public int teenside() {
+
+        Random dice = new Random();
+        return dice.nextInt(11)+1;
+    }
+
+    public boolean Attack(int combatValue) {
+        this.combatValue = combatValue;
+
+        if (combatValue < teenside()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+        
+    }
+
+
+
+
+
 }
